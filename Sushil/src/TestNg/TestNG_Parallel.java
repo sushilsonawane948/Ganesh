76d9d2448be6761af9428b1738e7166ccd8cd20e
sushilsonawane948@gmail.com <DUKKAR@DESKTOP-Q9LDSBS>
2021-05-21 14:05:14 +0530

    Java Projects

--- Sushil/src/TestNg/TestNG_Parallel.java
+++ Sushil/src/TestNg/TestNG_Parallel.java
@@ -4,36 +4,24 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 public class TestNG_Parallel {
+	
 	WebDriver driver;
-	@Test 
-	public void chromebrowser(){
+
+	@Test
+	public void chromebrowser() {
 		System.setProperty("webdriver.chrome.driver", "c:\\Users\\All driver files\\chromedriver.exe");
-		System.out.println("The thread ID for chrome is" +Thread.currentThread().getId());
-		 driver = new ChromeDriver();
-			System.out.println("Chrome Browser open succesfully");
-			driver.get("https://www.demoqa.com");
-String s="s";
-String s1="s";
-System.out.println(s.equals(s1));
-
-
-
-	}
-	@Test 
-	public void firefoxbrowser() {
-		
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\All driver files\\Gecko Driver\\geckodriver.exe");
-		System.out.println("The thread ID for firefox is" +Thread.currentThread().getId());
-		driver = new FirefoxDriver();
-		System.out.println("Firefox Browser open succesfully");
+		System.out.println("The thread ID for chrome is" + Thread.currentThread().getId());
+		driver = new ChromeDriver();
+		System.out.println("Chrome Browser open succesfully");
 		driver.get("https://www.demoqa.com");
+		String s = "s";
+		String s1 = "s";
+		System.out.println(s.equals(s1));
+
 	}
-/*
-	@Test 
+	@Test
 	public void openBrowser() {
 		driver.get("https://www.demoqa.com");
 		SoftAssert softassert = new SoftAssert();
@@ -42,9 +30,8 @@ System.out.println(s.equals(s1));
 
 		softassert.assertEquals(originalTitle, expectedTitle, "GET EXACT TEXT");
 		softassert.assertAll();
-	
-		System.out.println("Launching Google Chrome browser"); 
+
+		System.out.println("Launching Google Chrome browser");
 	}
-*/
 }
-}
+
