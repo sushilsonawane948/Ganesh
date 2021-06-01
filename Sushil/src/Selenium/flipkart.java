e0e41a65bc60c9af97f4714a1c6729233f0257b8
sushilsonawane948@gmail.com <DUKKAR@DESKTOP-Q9LDSBS>
2021-06-01 18:08:27 +0530

    Flipkart-Online Shopping Site

--- Sushil/src/Selenium/flipkart.java
+++ Sushil/src/Selenium/flipkart.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
 public class flipkart {
 
